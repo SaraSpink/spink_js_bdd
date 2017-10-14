2be0b21e1492ebe14cc8598bd4a63764151025b1
Sara Spink <spinkbot@gmail.com>
2017-10-14 15:32:56 -0600

    remove some commented out reference code

--- js/ageCalculator-interface.js
+++ js/ageCalculator-interface.js
@@ -12,35 +12,6 @@ $(document).ready(function() {
 
     $('#ageInSeconds').text(text).show()
 
-
   })
-  //
-  // $('#getInstrumentsForm').submit(function(event) {
-  //   event.preventDefault();
-  //   let store = $("input:radio[name=musicstore]:checked").val();
-  //   $('#beginning').slideUp()
-  //   if (store === "pawn") {
-  //     $('#pete').slideDown()
-  //   } else {
-  //     $('#archibald').slideDown()
-  //   };
-  // });
-  //
-  // $('#peteInstruments').submit(function(event) {
-  //   event.preventDefault();
-  //   let instrument = $("input:radio[name=instrument]:checked").val();
-  //   $('#pete').slideUp();
-  //   if (instrument !== "6") {
-  //     band.addInstrument(instrument)
-  //     $('#balance').text(band.addPoints(band.points));
-  //     $('#instruments').text(band.instruments);
-  //     $('#chords').text(band.chords)
-  //     $('#practiceBar').show();
-  //   }
-  //
-  //   if (band.addPoints(band.points) < 1){
-  //     $('#loser').show();
-  //   } else {
-  //     $('#practiceBar').show();
-  //   }
+  
 });
